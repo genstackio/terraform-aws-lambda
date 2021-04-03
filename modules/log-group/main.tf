57e6c019004722cefae363fa714e652db83ed575
Olivier Hoareau <oha@greenberets.io>
2021-04-03 19:42:33 +0200

    feat(log-group): ability to disable

--- modules/log-group/main.tf
+++ modules/log-group/main.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudwatch_log_group" "lambda" {
+  count             = var.enabled ? 1 : 0
   name              = "/aws/lambda/${var.name}"
   retention_in_days = var.retention
 }
--- modules/log-group/variables.tf
+++ modules/log-group/variables.tf
@@ -1,6 +1,10 @@
 variable "name" {
   type = string
 }
+variable "enabled" {
+  type    = bool
+  default = true
+}
 variable "retention" {
   type    = number
   default = 14
