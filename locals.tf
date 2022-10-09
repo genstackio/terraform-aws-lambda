b0af0ff2aa0cfc6134c3e4641f69bbd879849094
Olivier Hoareau <oha@greenberets.io>
2022-10-09 18:00:18 +0200

    fix: fix

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   is_image = (null == var.file) && (null != var.image)
   is_s3    = (null == var.file) && !local.is_image && (null != var.s3_bucket)
   is_managed_s3 = local.is_s3 && ("=" == substr(var.s3_key, 0, 1))
-  real_s3_key = local.is_s3 ? (local.is_managed_s3 ? substr(var.s3_key, 1) : var.s3_key) : var.s3_key
+  real_s3_key = local.is_s3 ? (local.is_managed_s3 ? substr(var.s3_key, 1, length(var.s3_key) - 1) : var.s3_key) : var.s3_key
 }
